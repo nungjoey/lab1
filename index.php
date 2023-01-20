7577b458558d67a6cf36d28c1c6adbf3f6f6ba25
Kuntapat777 <106942441+Kuntapat777@users.noreply.github.com>
2023-01-20 19:15:48 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form action="show.php">
+        your name:<input type="text" name="name">.<br>
+        your age:<input type="text" name="age">.<br>
+        your gender:
+        <input type="radio" name="gender" value="female">woman
+        <input type="radio" name="gender" value="male">man<br>
+        your status:
+        <input type="radio" name="status" value="unmarried">unmarried
+        <input type="radio" name="status" value="married">married<br>
+        <input type="submit" value="Submit"><br>
+    </form>
+</body>
+</html>
