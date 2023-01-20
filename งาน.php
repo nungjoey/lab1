3258694fd2ba9c008cef35c5bd4a1672e0972c39
Kuntapat777 <106942441+Kuntapat777@users.noreply.github.com>
2023-01-20 19:12:11 +0700

    Add files via upload

---
+++ งาน.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    $name =$_GET["name"];
+    $age=$_GET["age"];
+    $gender=$_GET["gender"];
+    $status=$_GET["status"];
+    if ($age < "15" && $gender == "male" && $status == "unmarried"){
+        $titaltxt="DC.";
+        $gender = "Male";
+        $status="unmarried";
+    }else if($age > "14" && $gender == "male" && $status == "unmarried"){
+        $titaltxt="Mr.";
+        $gender = "Male";
+        $status="unmarried";
+    }else if($age > "14" && $gender == "male" && $status == "married"){
+        $titaltxt="Mr.";
+        $gender = "Male";
+        $status="unmarried";
+    }else if($age < "15" && $gender == "male" && $status == "married"){
+        $titaltxt="DC.";
+        $gender = "Male";
+        $status="unmarried";
+    }else if($age < "15" && $gender == "female" && $status == "unmarried"){
+        $titaltxt="girl.";
+        $gender = "female";
+        $status="unmarried";
+    }else if($age > "14" && $gender == "female" && $status == "unmarried"){
+        $titaltxt="ms.";
+        $gender = "female";
+        $status="unmarried";
+    }else if($age > "14" && $gender == "female" && $status == "married"){
+        $titaltxt="Mrs.";
+        $gender = "female";
+        $status="married";
+    }else if($age < "15" && $gender == "female" && $status == "married"){
+        $titaltxt="girl.";
+        $gender = "female";
+        $status="married";
+    }
+    echo "<font style='font-size:24px'>your information</font>"."<br>";
+    echo "name:".$titaltxt.$name."<br>";
+    echo "age:".$age."<br>";
+    echo "sex:".$gender."<br>";
+    echo "status:".$status."<br>";
+    ?>
+</body>
+
+</html>
